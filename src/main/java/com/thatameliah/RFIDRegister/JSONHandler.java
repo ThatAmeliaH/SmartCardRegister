7e726041e19a438e31996a99ed31827d085fbaca
Amelia School <243829@G106-11149>
2025-10-10 12:44:39 +0100

    Initial commit

---
+++ src/main/java/com/thatameliah/RFIDRegister/JSONHandler.java
@@ -0,0 +1,38 @@
+﻿package com.thatameliah.nfcregister;
+
+import org.json.JSONObject;
+
+import java.io.BufferedWriter;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.*;
+
+public class JSONHandler {
+    public JSONObject NewUser(String name, String id) {
+        Map<String, String> hash = new HashMap<>();
+        hash.put("name", name);
+        hash.put("id", id);
+        return new JSONObject(hash);
+    }
+
+    public void WriteJsonObjectsToFile(JSONObject[] objects, String filename, String extension) {
+        try(BufferedWriter writer = new BufferedWriter(new FileWriter("saves/"+filename+extension))) {
+            for (JSONObject object : objects) {
+                writer.write(String.valueOf(object));
+            }
+        } catch (IOException err) {
+            System.out.println("Error writing to file "+filename+extension+": "+err.getMessage());
+        }
+    }
+
+    public void DeleteRegister(String filename, String extension) {
+        Path path = Paths.get("saves/"+filename+extension);
+        try { Files.delete(path); }
+        catch(IOException err) {
+            System.out.println("Failed to delete file "+filename+extension+": "+err.getMessage());
+        }
+    }
+}
---
+++ src/main/java/com/thatameliah/RFIDRegister/Main.java
@@ -0,0 +1,7 @@
+package com.thatameliah.nfcregister;
+
+public class Main {
+    public static void main(String[] args) {
+        
+    }
+}
